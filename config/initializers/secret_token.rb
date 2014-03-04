f66aa7faa0ba233f8ad933d711e80de9ff2b8ca5
Ray Wu <rayw@soundofhope.org>
2014-03-04 15:11:04 -0800

    $rvm use ruby-2.1.1@learn-rails --create $gem install rails $rails new learn-rails $cd learn-rails $rvm use ruby-2.1.1@learn-rails --ruby-version $git init . $git add . $git commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = 'df5bbf13c485bf0641f45e40a2a251b045a855279114f34a05cd986ff7907789f701069e42dde5e085b2580b36d075b1b3f4c7de97de7a4b53ad4e86c10dcd23'
